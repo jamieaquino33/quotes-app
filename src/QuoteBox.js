7b900242d8409e91096dcff04c5154d68075348a
Jamie Aquino <jamie.aquino33@gmail.com>
2019-11-04 13:48:12 -0600

    Added in list of quotes. Set up the QuoteBox component to use quote props passed in. Added functionality to generate and show a new random quote

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -4,7 +4,9 @@ class QuoteBox extends Component {
     render() {
         return (
             <div>
-            <h1>hi</h1>
+            <h1>{this.props.quote}</h1>
+            <h1>{this.props.author}</h1>
+            <button onClick={this.props.handleClick}>New Quote</button>
             </div>
         );
     }
